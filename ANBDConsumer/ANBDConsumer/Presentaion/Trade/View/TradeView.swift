1e5fa9bb5fe517c95d1b7bebdb962fa470998e52
JURI <80569323+juri123123@users.noreply.github.com>
2024-04-05 14:31:21 +0900

    feat: categoryDividerView

--- ANBDConsumer/ANBDConsumer/Presentaion/Trade/View/TradeView.swift
+++ ANBDConsumer/ANBDConsumer/Presentaion/Trade/View/TradeView.swift
@@ -5,4 +5,34 @@
 //  Created by 최주리 on 4/3/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TradeView: View {
+    @State var category: Category = .nanua
+    
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            VStack {
+                CategoryDividerView(category: $category)
+            }//VStack
+            
+            Button(action: {
+                
+            }, label: {
+                
+            })
+        }//ZStack
+        .navigationTitle("나눔 · 거래")
+        .toolbarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Image(systemName: "magnifyingglass")
+            }
+        }
+    }
+}
+
+
+#Preview {
+    TradeView(category: .nanua)
+}

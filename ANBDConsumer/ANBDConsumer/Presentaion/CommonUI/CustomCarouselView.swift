c6b7b176c7985eecf49b02c148ccea8cd95b2607
최정인 <wjddls980912@naver.com>
2024-04-05 16:24:13 +0900

    feat: HomeView UI 구현

---
+++ ANBDConsumer/ANBDConsumer/Presentaion/CommonUI/CustomCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCarouselView.swift
+//  ANBDConsumer
+//
+//  Created by 최정인 on 4/5/24.
+//
+
+import SwiftUI
+
+struct CustomCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCarouselView()
+}

21076f0099bf0b8c7eed7b7413c0a8ccb98d2cd7
Jiho <yjh7827@naver.com>
2024-04-05 16:07:07 +0900

    fix: Location rawvalue 추가, User 관련 enum 파일 분리

---
+++ ANBDModel/Sources/Domain/Enum/Location.swift
@@ -0,0 +1,28 @@
+//
+//  Location.swift
+//
+//
+//  Created by 유지호 on 4/5/24.
+//
+
+import Foundation
+
+public enum Location: String, Codable {
+    case seoul = "서울"
+    case gyeonggi = "경기"
+    case incheon = "인천"
+    case gwangju = "광주"
+    case busan = "부산"
+    case dague = "대구"
+    case daejeon = "대전"
+    case ulsan = "울산"
+    case gangwon = "강원"
+    case gyeongBuk = "경북"
+    case gyeongNam = "경남"
+    case jeonBuk = "전북"
+    case jeonNam = "전남"
+    case chungBuk = "충북"
+    case chungNam = "충남"
+    case sejong = "세종"
+    case jeju = "제주"
+}

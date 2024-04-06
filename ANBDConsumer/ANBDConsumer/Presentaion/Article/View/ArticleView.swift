86802643ce635d8378f2cb0375ab8d54c6bd8901
rlvy0513 <rlvy0513@naver.com>
2024-04-06 16:59:06 +0900

    refactor: Community -> Article로 변경

---
+++ ANBDConsumer/ANBDConsumer/Presentaion/Article/View/ArticleView.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleView.swift
+//  ANBDConsumer
+//
+//  Created by 기 표 on 4/6/24.
+//
+
+import SwiftUI
+
+struct ArticleView: View {
+    var body: some View {
+        Text("ArticleView")
+    }
+}
+
+#Preview {
+    ArticleView()
+}

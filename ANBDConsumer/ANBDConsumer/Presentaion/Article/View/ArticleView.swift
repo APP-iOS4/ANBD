710d19f93608ed50effe3d284e0370dadddd7bb5
rlvy0513 <rlvy0513@naver.com>
2024-04-07 14:27:20 +0900

    feat: ArticleCreateView UI 작업

--- ANBDConsumer/ANBDConsumer/Presentaion/Article/View/ArticleView.swift
+++ ANBDConsumer/ANBDConsumer/Presentaion/Article/View/ArticleView.swift
@@ -8,8 +8,64 @@
 import SwiftUI
 
 struct ArticleView: View {
+    
+    @State private var isShowingCreateView: Bool = false
+    @State var category: Category = .accua
+    
+//    private var filteredList: [Int] {
+//        switch category {
+//        case .nanua: [1, 2, 3, 4, 5, 6]
+//        case .baccua: [1, 2, 3, 4, 5, 6]
+//        case .accua: [1, 2, 3, 4, 5, 6]
+//        case .dasi: [1, 2, 3, 4, 5, 6]
+//        }
+//    }
+    
     var body: some View {
-        Text("ArticleView")
+        NavigationStack {
+            ZStack(alignment: .bottomTrailing) {
+                VStack {
+                    CategoryDividerView(category: $category)
+                    
+                    TabView(selection: $category) {
+                        articleListView
+                            .tag(Category.accua)
+                        articleListView
+                            .tag(Category.dasi)
+                    }
+                    .tabViewStyle(.page(indexDisplayMode: .never))
+                    .ignoresSafeArea()
+                }
+                
+                Button {
+                    self.isShowingCreateView.toggle()
+                } label: {
+                    WriteButtonView()
+                }
+            }
+        }
+        .navigationTitle("정보 공유")
+        .toolbarTitleDisplayMode(.inline)
+        .fullScreenCover(isPresented: $isShowingCreateView, content: {
+            ArticleCreateView(flag: $category, isShowingCreateView: $isShowingCreateView)
+        })
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Image(systemName: "magnifyingglass")
+            }
+        }
+    }
+}
+
+extension ArticleView {
+    var articleListView: some View {
+        ScrollView {
+            LazyVStack {
+//                ForEach(filteredList, id: \.self) { article in
+//                        NavigationLink(value: , label: )
+//                }
+            }
+        }
     }
 }
 

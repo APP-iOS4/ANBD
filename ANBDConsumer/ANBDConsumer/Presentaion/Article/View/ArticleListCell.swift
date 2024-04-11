0a1e38adfdaed5cfef141b2b5bac11185b4769eb
rlvy0513 <rlvy0513@naver.com>
2024-04-11 21:37:35 +0900

    refactor: ArticleDetailView QA 반영
    ArticleCreateView: 제목 fontWeight -> Bold 로 수정
    ArticleDetailView: 텍스트필드 글씨 크기 수정, 댓글 미 입력시 전송 버튼 안 눌리게 수정, 제목 fontWeight -> Bold 로 수정, 좋아요 버튼 크기 텍스트 크기 조정, 댓글부분 텍스트 크기 조정

--- ANBDConsumer/ANBDConsumer/Presentaion/Article/View/ArticleListCell.swift
+++ ANBDConsumer/ANBDConsumer/Presentaion/Article/View/ArticleListCell.swift
@@ -13,21 +13,10 @@ struct ArticleListCell: View {
     
     var body: some View {
         HStack(alignment: .top) {
-            Image("\(article.imagePaths.first ?? "")")
-//                .resizable()
-//                .scaledToFit()
-//                .clipShape(RoundedRectangle(cornerRadius: 10))
-//                .frame(width: 110, height: 110)
-//                .padding(.trailing, 10)
-//                .resizable()
-//                .aspectRatio(contentMode: .fit)
-//                .clipShape(RoundedRectangle(cornerRadius: 10))
-//                .frame(width: 100, height: 100)
-//                .padding(.trailing, 10)
-            
+            Image("\(article.imagePaths.first ?? "DummyPuppy1")")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .frame(width: 110, height: 110)
+                .frame(width: 100, height: 100)
                 .clipShape(RoundedRectangle(cornerRadius: 10))
                 .padding(.trailing, 10)
             
@@ -37,8 +26,7 @@ struct ArticleListCell: View {
                     .foregroundStyle(.gray900)
                     .lineLimit(2)
                     .multilineTextAlignment(.leading)
-                    .padding(.top, 15)
-
+                
                 HStack {
                     Text("\(article.writerNickname)")
                     Text("・")
@@ -47,81 +35,37 @@ struct ArticleListCell: View {
                         .padding(.leading, -5)
                     Spacer()
                 }
+                .lineLimit(1)
                 .font(ANBDFont.Caption3)
                 .foregroundStyle(.gray400)
-                /*
-                ZStack {
-                    VStack {
-                        HStack {
-                            Spacer()
-                            Image(systemName: "hand.thumbsup")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 16, height: 16)
-                                .padding(.trailing, 5) // 아이콘과 텍스트 사이 간격 조정
-                            
-                                                    Text("\(article.likeCount)")
-//                            Text("99119")
-                                .font(.system(size: 12))
-                                .multilineTextAlignment(.leading) // 텍스트를 왼쪽 정렬
-
-                        }
-                        
-                        HStack {
-                            Spacer()
-                            Image(systemName: "text.bubble")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 15, height: 15)
-                                .padding(.top, 3)
-                                .padding(.trailing, 5)
-                            
-                                                    Text("\(article.commentCount)")
-//                            Text("912312399")
-                                .font(.system(size: 12))
-                                .padding(.top, 3)
-                                .multilineTextAlignment(.leading)
-                            
-                        }
-                        .padding(.bottom, 12)
-                    }
-                    .foregroundStyle(.gray800)
-                }
-                .frame(height: 35)
-                */
-
-                VStack {
-                    HStack {
-                        Spacer()
-                        Image(systemName: "hand.thumbsup")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 16, height: 16)
-                        
-                        Text("\(article.likeCount)")
-                            .font(.system(size: 12))
-                            .padding(.trailing, 10)
-                            .padding(.top, 2)
-                        
-                        
-                        Image(systemName: "text.bubble")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 15, height: 15)
-                        
-                        Text("\(article.commentCount)")
-                            .font(.system(size: 12))
-                            .padding(.top, 2)
-
-                    }
+                Spacer(minLength: 0)
+                
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "hand.thumbsup")
+                        .resizable()
+                        .frame(width: 16, height: 16)
+                    
+                    Text("\(article.likeCount)")
+                        .font(.system(size: 12))
+                        .padding(.trailing, 10)
+                        .padding(.top, 2)
+                    
+                    Image(systemName: "text.bubble")
+                        .resizable()
+                        .frame(width: 15, height: 15)
+                    
+                    Text("\(article.commentCount)")
+                        .font(.system(size: 12))
+                        .padding(.top, 2)
                 }
                 .foregroundStyle(.gray800)
-                .padding(.top, 18)
             }
         }
+        .frame(height: 100)
     }
 }
-
 #Preview {
     ArticleListCell(article: Article(writerID: "writerID", writerNickname: "닉네임1", category: .accua, title: "제목제목", content: "내용내용"))
 }

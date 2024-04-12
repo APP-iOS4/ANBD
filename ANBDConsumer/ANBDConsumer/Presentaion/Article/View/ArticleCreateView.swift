9f43d6eea1019f7a4a360b6bc41e6f18bb2ff602
rlvy0513 <rlvy0513@naver.com>
2024-04-12 10:28:28 +0900

    refactor: ArticleCreateView QA 반영
    title, content 수정 후 취소 누르면 커스텀 알럿 띄우기

--- ANBDConsumer/ANBDConsumer/Presentaion/Article/View/ArticleCreateView.swift
+++ ANBDConsumer/ANBDConsumer/Presentaion/Article/View/ArticleCreateView.swift
@@ -18,172 +18,218 @@ struct ArticleCreateView: View {
     @State private var title: String = ""
     @State private var content : String = ""
     @State var placeHolder : String = "ANBD 이용자들을 위해 여러분들의 아껴쓰기 / 다시쓰기 Tip을 전수해주세요!"
-
+    
     @State private var isShowingImageAlert: Bool = false
     @State private var selectedItems: [PhotosPickerItem] = []
     @State private var selectedImageData: [Data] = []
     
     @State private var selectedMenuText: String = "아껴쓰기"
     
+    @State private var isShowingEditAlert: Bool = false
+    
+    @Environment(\.dismiss) private var dismiss
+    
     var isNewArticle: Bool
     var article: Article?
     
-
-
+    
+    
     var body: some View {
-        NavigationStack {
-            VStack {
-                InstructionsView()
-                
+        ZStack {
+            NavigationStack {
                 VStack {
-                    TextField("제목을 입력하세요", text: $title)
-                        .onAppear() {
-                            if !isNewArticle {
-                                if let article = article {
-                                    self.title = article.title
-                                }
-                            }
-                        }
-                        .font(ANBDFont.pretendardBold(24))
-                        .padding(.leading, 20)
-                    Divider()
-                        .padding(.horizontal, 20)
+                    InstructionsView()
                     
-                    ZStack(alignment: .topLeading) {
-                        if content.isEmpty {
-                            Text(placeHolder)
-                                .foregroundStyle(.gray400)
-                                .font(ANBDFont.body1)
-                                .padding(.leading , 20)
-                                .padding(.top , 8)
-                        }
-                        TextEditor(text: $content)
-                            .scrollContentBackground(.hidden)
-                            .font(ANBDFont.body1)
-                            .padding(.leading, 15)
+                    VStack {
+                        TextField("제목을 입력하세요", text: $title)
                             .onAppear() {
                                 if !isNewArticle {
                                     if let article = article {
-                                        self.content = article.content
+                                        self.title = article.title
                                     }
                                 }
                             }
-                    }
-                    ScrollView(.horizontal) {
-                        HStack {
-                            ForEach(selectedImageData, id: \.self) {
-                                photoData in
-                                ZStack(alignment:.topTrailing) {
-                                    if let image = UIImage(data: photoData) {
-                                        Image(uiImage: image)
-                                            .resizable()
-                                            .frame(width : 70 , height: 70)
-                                            .cornerRadius(10)
-                                            .clipped()
-                                            .padding(10)
-                                    }
-                                    
-                                    Button {
-                                        if let idx = selectedImageData.firstIndex(of: photoData) {
-                                            selectedImageData.remove(at: idx)
+                            .font(ANBDFont.pretendardBold(24))
+                            .padding(.leading, 20)
+                        Divider()
+                            .padding(.horizontal, 20)
+                        
+                        ZStack(alignment: .topLeading) {
+                            if content.isEmpty {
+                                Text(placeHolder)
+                                    .foregroundStyle(.gray400)
+                                    .font(ANBDFont.body1)
+                                    .padding(.leading , 20)
+                                    .padding(.top , 8)
+                            }
+                            TextEditor(text: $content)
+                                .scrollContentBackground(.hidden)
+                                .font(ANBDFont.body1)
+                                .padding(.leading, 15)
+                                .onAppear() {
+                                    if !isNewArticle {
+                                        if let article = article {
+                                            self.content = article.content
                                         }
-                                    } label: {
-                                        Circle()
-                                            .overlay (
-                                                Image(systemName: "xmark.circle.fill")
-                                                    .font(ANBDFont.pretendardSemiBold(20))
-                                                    .foregroundStyle(.gray900)
-                                            )
-                                            .foregroundStyle(.gray50)
-                                            .frame(width: 20, height:20)
                                     }
                                 }
-                            }
                         }
                         
-                    }
-                }
-                Divider()
-                
-                HStack {
-                    if selectedImageData.count == 5 {
-                        Button(action: {
-                            isShowingImageAlert.toggle()
-                        }, label: {
-                            Image(systemName: "photo")
-                            Text("사진")
-                        })
-                        .foregroundStyle(.accent)
-                    } else {
-                        PhotosPicker(
-                            selection: $selectedItems, maxSelectionCount: 5-selectedImageData.count,
-                            matching: .images
-                        ) {
-                            Image(systemName: "photo")
-                            Text("사진")
+                        ScrollView(.horizontal) {
+                            HStack {
+                                ForEach(selectedImageData, id: \.self) {
+                                    photoData in
+                                    ZStack(alignment:.topTrailing) {
+                                        if let image = UIImage(data: photoData) {
+                                            Image(uiImage: image)
+                                                .resizable()
+                                                .frame(width : 70 , height: 70)
+                                                .cornerRadius(10)
+                                                .clipped()
+                                                .padding(10)
+                                        }
+                                        
+                                        Button {
+                                            if let idx = selectedImageData.firstIndex(of: photoData) {
+                                                selectedImageData.remove(at: idx)
+                                            }
+                                        } label: {
+                                            Circle()
+                                                .overlay (
+                                                    Image(systemName: "xmark.circle.fill")
+                                                        .font(ANBDFont.pretendardSemiBold(20))
+                                                        .foregroundStyle(.gray900)
+                                                )
+                                                .foregroundStyle(.gray50)
+                                                .frame(width: 20, height:20)
+                                        }
+                                    }
+                                }
+                            }
+                            
                         }
-                        .foregroundStyle(.accent)
                     }
-                    Spacer()
+                    Divider()
+                    
+                    HStack {
+                        if selectedImageData.count == 5 {
+                            Button(action: {
+                                isShowingImageAlert.toggle()
+                            }, label: {
+                                Image(systemName: "photo")
+                                Text("사진")
+                            })
+                            .foregroundStyle(.accent)
+                        } else {
+                            PhotosPicker(
+                                selection: $selectedItems, maxSelectionCount: 5-selectedImageData.count,
+                                matching: .images
+                            ) {
+                                Image(systemName: "photo")
+                                Text("사진")
+                            }
+                            .foregroundStyle(.accent)
+                        }
+                        Spacer()
+                    }
+                    .frame(height: 40)
+                    .padding(.leading, 10)
                 }
-                .frame(height: 40)
-                .padding(.leading, 10)
-            }
-            .toolbar {
-                ToolbarItem(placement: .primaryAction) {
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button {
+                            //                        Task {
+                            //                            let newArticle = Article(title: $title, content: $content, category: $flag)
+                            //                            try await articleViewModel.writeArticle(article: newArticle, imageDatas: selectedImageData)
+                            //                        }
+                            isShowingCreateView.toggle()
+                        } label: {
+                            Text("완료")
+                        }
+                        .disabled(title.isEmpty || content.isEmpty)
+                    }
+                    
+                    //                    ToolbarItem(placement: .cancellationAction) {
+                    //                        Button {
+                    //                            if !isNewArticle {
+                    //                                if let article = article {
+                    //                                    isShowingEditAlert.toggle()
+                    //                                    print("변경함")
+                    //                                }
+                    //
+                    //                            } else {
+                    //                                isShowingCreateView.toggle()
+                    //                            }
+                    //                        } label: {
+                    //                            Text("취소")
+                    //                        }
+                    //                    }
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button {
+                            if !isNewArticle {
+                                if let article = article {
+                                    // 이전 값과 새로운 값 비교
+                                    let hasTitleChanged = title != article.title
+                                    let hasContentChanged = content != article.content
+                                    
+                                    // 변경이 있다면 수정 경고 표시
+                                    if hasTitleChanged || hasContentChanged {
+                                        isShowingEditAlert.toggle()
+                                    } else {
+                                        // 변경이 없다면 뷰 닫기
+                                        isShowingCreateView.toggle()
+                                    }
+                                }
+                            } else {
+                                // 새 게시물인 경우 뷰 닫기
+                                isShowingCreateView.toggle()
+                            }
+                        } label: {
+                            Text("취소")
+                        }
+                    }
+                }
+                .toolbarTitleMenu {
                     Button {
-//                        Task {
-//                            let newArticle = Article(title: $title, content: $content, category: $flag)
-//                            try await articleViewModel.writeArticle(article: newArticle, imageDatas: selectedImageData)
-//                        }
-                        isShowingCreateView.toggle()
+                        category = .accua
+                        selectedMenuText = "아껴쓰기"
                     } label: {
-                        Text("완료")
+                        Text("아껴쓰기")
                     }
-                    .disabled(title.isEmpty || content.isEmpty)
-                }
-                
-                ToolbarItem(placement: .cancellationAction) {
-                    Button(role: .cancel) {
-                        isShowingCreateView.toggle()
+                    
+                    Button {
+                        category = .dasi
+                        selectedMenuText = "다시쓰기"
                     } label: {
-                        Text("취소")
+                        Text("다시쓰기")
                     }
                 }
-            }
-            .toolbarTitleMenu {
-                Button {
-                    category = .accua
-                    selectedMenuText = "아껴쓰기"
-                } label: {
-                    Text("아껴쓰기")
-                }
-                
-                Button {
-                    category = .dasi
-                    selectedMenuText = "다시쓰기"
-                } label: {
-                    Text("다시쓰기")
-                }
-            }
-            .onChange(of: selectedItems) {
-                for newItem in selectedItems {
-                    Task {
-                        if let data = try? await newItem.loadTransferable(type: Data.self) {
-                            selectedImageData.append(data)
-                        }
-                        if selectedImageData.count > 5 {
-                            selectedImageData.removeLast()
+                .onChange(of: selectedItems) {
+                    for newItem in selectedItems {
+                        Task {
+                            if let data = try? await newItem.loadTransferable(type: Data.self) {
+                                selectedImageData.append(data)
+                            }
+                            if selectedImageData.count > 5 {
+                                selectedImageData.removeLast()
+                            }
                         }
                     }
+                    selectedItems = []
                 }
-                selectedItems = []
+                .alert("이미지는 최대 5장만 가능합니다", isPresented: $isShowingImageAlert) {
+                    Button("확인", role: .cancel) { }
+                }
+                .navigationTitle(category == .accua ? "아껴쓰기" : "다시쓰기")
+                .navigationBarTitleDisplayMode(.inline)
             }
-            .alert("이미지는 최대 5장만 가능합니다", isPresented: $isShowingImageAlert) {
-                Button("확인", role: .cancel) { }
+            if isShowingEditAlert {
+                CustomAlertView(isShowingCustomAlert: $isShowingEditAlert, viewType: .leaveChatRoom) {
+                    dismiss()
+                }
+                .zIndex(1)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
-            .navigationTitle(category == .accua ? "아껴쓰기" : "다시쓰기")
-            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }

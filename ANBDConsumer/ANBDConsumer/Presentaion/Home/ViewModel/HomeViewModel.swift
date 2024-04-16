bb8af893d6c6b806d3f1120539c10f352e2d0288
Jungin Choi <37467592+choijungp@users.noreply.github.com>
2024-04-16 11:31:33 +0900

    feature: HomeView Model 연결 (#149)
    * feat: HomeView - Article 연결
    
    * feat: HomeView - Trade 연결
    
    * feat: Banner 불러오기 및 HomeView 리팩토링
    
    * fix: @ViewBuilder 삭제 (코드리뷰 반영)

--- ANBDConsumer/ANBDConsumer/Presentaion/Home/ViewModel/HomeViewModel.swift
+++ ANBDConsumer/ANBDConsumer/Presentaion/Home/ViewModel/HomeViewModel.swift
@@ -9,99 +9,66 @@ import Foundation
 import SwiftUI
 import ANBDModel
 
+@MainActor
 final class HomeViewModel: ObservableObject {
+    private let storageManager = StorageManager.shared
+    private let articleUsecase: ArticleUsecase = DefaultArticleUsecase()
+    private let tradeUsecase: TradeUsecase = DefaultTradeUsecase()
+    private let bannerUsecase: BannerUsecase = DefaultBannerUsecase()
+    
     @Published var homePath: NavigationPath = NavigationPath()
     
-    @Published var bannerItemList: [BannerItem] = BannerItem.mockData
-    @Published var blogURL: String = "https://blog.naver.com/PostView.naver?blogId=mesns&logNo=223382133878&categoryNo=15&parentCategoryNo=14&viewDate=&currentPage=3&postListTopCurrentPage=1&from=postList&userTopListOpen=true&userTopListCount=5&userTopListManageOpen=false&userTopListCurrentPage=3"
-    @Published var isShowingWebView: Bool = false
+    @Published var bannerItemList: [Banner] = []
     
-
-    @Published var accuaArticle: Article = .init(writerID: "rlvy123",
-                                                 writerNickname: "기표짱표",
-                                                 category: .accua,
-                                                 title: "아껴쓰기 타이틀 무순 내용이게용 ~~ 메롱 메롱",
-                                                 content: "",
-                                                 imagePaths: ["DummyPuppy2"])
+    @Published var accuaArticle: Article = .init(writerID: "", writerNickname: "", category: .accua, title: "",content: "", imagePaths: [])
+    @Published var dasiArticle: Article = .init(writerID: "", writerNickname: "", category: .accua, title: "",content: "", imagePaths: [])
     
-    @Published var dasiArticle: Article = .init(writerID: "jullilll",
-                                                 writerNickname: "주리줄이",
-                                                category: .dasi,
-                                                 title: "다시쓰기 타이틀 ~~~ 울라울라",
-                                                 content: "",
-                                                imagePaths: ["DummyPuppy4"])
+    @Published var nanuaTrades: [Trade] = []
+    @Published var baccuaTrades: [Trade] = []
     
-    @Published var nanuaTrades: [Trade] = [.init(writerID: "maru",
-                                                 writerNickname: "마루마루",
-                                                 category: .nanua,
-                                                 itemCategory: .beautyCosmetics,
-                                                 location: .busan,
-                                                 title: "나눠쓰기마루마루",
-                                                 content: "",
-                                                 myProduct: "",
-                                                 imagePaths: ["DummyPuppy3"]),
-                                           .init(writerID: "maru",
-                                                 writerNickname: "마루마루",
-                                                 category: .nanua,
-                                                 itemCategory: .beautyCosmetics,
-                                                 location: .busan,
-                                                 title: "나눠쓰기메롱메롱",
-                                                 content: "",
-                                                 myProduct: "",
-                                                 imagePaths: ["DummyPuppy1"]),
-                                           .init(writerID: "maru",
-                                                 writerNickname: "마루마루",
-                                                 category: .nanua,
-                                                 itemCategory: .beautyCosmetics,
-                                                 location: .busan,
-                                                 title: "나눠쓰기어쩔어쩔",
-                                                 content: "",
-                                                 myProduct: "",
-                                                 imagePaths: ["DummyPuppy2"]),
-                                           .init(writerID: "maru",
-                                                 writerNickname: "마루마루",
-                                                 category: .nanua,
-                                                 itemCategory: .beautyCosmetics,
-                                                 location: .busan,
-                                                 title: "나눠쓰기어쩔어쩔",
-                                                 content: "",
-                                                 myProduct: "",
-                                                 imagePaths: ["DummyPuppy4"]),]
-    @Published var baccuaTrades: [Trade] = [.init(writerID: "maru",
-                                                  writerNickname: "마루마루",
-                                                  category: .baccua,
-                                                  itemCategory: .beautyCosmetics,
-                                                  location: .busan,
-                                                  title: "바꿔쓰기마루마루",
-                                                  content: "",
-                                                  myProduct: "키보드",
-                                                  imagePaths: ["DummyPuppy3"]),
-                                            .init(writerID: "maru",
-                                                  writerNickname: "마루마루",
-                                                  category: .baccua,
-                                                  itemCategory: .beautyCosmetics,
-                                                  location: .busan,
-                                                  title: "바꿔쓰기메롱메롱",
-                                                  content: "",
-                                                  myProduct: "뿡이다요",
-                                                  imagePaths: ["DummyPuppy1"]),]
-}
-
-struct BannerItem: Identifiable {
-    private(set) var id: UUID = .init()
-    var title: String
-    var imageStirng: String
-    var url: String
     
-    static let mockData: [BannerItem] = [
-        .init(title: "3월 22일은 물의 소중함을 알리는 세계 물의 날💧",
-              imageStirng: "https://blogfiles.pstatic.net/MjAyNDAzMDhfMjQy/MDAxNzA5ODU3OTU1MDky.7gM8iDEIYO2OCxGeDnPeimUO0EsiWBdAlQScxUo2Dt4g.BBQWmIp3latdi2jbPeGwY_jWyx_erJ4D_awsanupK7Ug.PNG/0306_%EC%84%B8%EA%B3%84%EB%AC%BC%EC%9D%98%EB%82%A0(%EB%8C%80%EC%A7%80)-02.png",
-              url: "https://blog.naver.com/PostView.naver?blogId=mesns&logNo=223382133878&categoryNo=15&parentCategoryNo=14&viewDate=&currentPage=3&postListTopCurrentPage=1&from=postList&userTopListOpen=true&userTopListCount=5&userTopListManageOpen=false&userTopListCurrentPage=3"),
-        .init(title: "국립공원의 가치를 알리는 3월 3일은 국립공원의 날🌱",
-              imageStirng: "https://postfiles.pstatic.net/MjAyNDAyMjdfMTYw/MDAxNzA5MDAwMzY3ODM0._TKSrBBaq8m3HG5Kr2mu-FqV0U3YeO-H64hmYeCVVOkg.xLED3n6xklUBKxEHfyqskx2c6gSmX72hpGaKiv_6ugIg.JPEG/%EB%82%A0%EC%A2%80%EB%B3%B4%EC%86%8C_%EC%9D%B8%EC%8A%A4%ED%83%80v2_02_04.jpg?type=w466",
-              url: "https://blog.naver.com/PostView.naver?blogId=mesns&logNo=223368880110&categoryNo=1&parentCategoryNo=14&viewDate=&currentPage=5&postListTopCurrentPage=1&from=postList&userTopListOpen=true&userTopListCount=5&userTopListManageOpen=false&userTopListCurrentPage=5"),
-        .init(title: "지구를 지키는 여행법 - 사용하지 않는 콘센트 뽑기",
-              imageStirng: "https://postfiles.pstatic.net/MjAyMzA4MDFfNTIg/MDAxNjkwODU2MzAyMzIw.bSisI6_iO4tnvf2T5o8djB5jBUu0-zURf4pQkWwHcRQg.4jdARftzPn0IBh5IUHa23eT85w2zFgBhBehuA9UswiQg.PNG.mesns/05.png?type=w966",
-              url: "https://blog.naver.com/PostView.naver?blogId=mesns&logNo=223171647347&categoryNo=27&parentCategoryNo=27&from=thumbnailList")
-    ]
+    /// 광고 · 배너 가져오기
+    func loadBanners() async {
+        do {
+            try await bannerItemList = bannerUsecase.loadBannerList()
+        } catch {
+            
+        }
+    }
+    
+    /// 아껴쓰기 · 다시쓰기 최신 1개씩 가져오기
+    func loadArticle(category: ANBDCategory) async {
+        do {
+            if category == .accua {
+                try await accuaArticle = articleUsecase.loadRecentArticle(category: .accua)
+            } else if category == .dasi {
+                try await dasiArticle = articleUsecase.loadRecentArticle(category: .dasi)
+            }
+        } catch {
+            
+        }
+    }
+    
+    /// 나눠쓰기 · 바꿔쓰기 최신순으로 각각 4개, 2개씩 가져오기
+    func loadTrades(category: ANBDCategory) async {
+        do {
+            if category == .nanua {
+                try await nanuaTrades = tradeUsecase.loadRecentTradeList(category: .nanua)
+            } else if category == .baccua {
+                try await baccuaTrades = tradeUsecase.loadRecentTradeList(category: .baccua)
+            }
+        } catch {
+            
+        }
+    }
+    
+    /// 이미지 로딩
+//    func loadImage(path: StoragePath, containerID: String, imagePath: String) -> Data {
+//        do {
+//            return try await storageManager.downloadImage(path: path, containerID: containerID, imagePath: imagePath)
+//        } catch {
+//            
+//        }
+//        return Data()
+//    }
 }

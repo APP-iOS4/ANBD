aefd4b6fbce3e73faa8da08c05c004bb4a5d6adf
Seongmin Kim <sjybext@naver.com>
2024-04-17 15:55:32 +0900

    feat: 로그아웃, 회원탈퇴 및 회원가입 부가 기능 추가 완료 (#164)
    * style: 컨벤션에 맞게 부울 값 변수 이름 수정
    
    * feat: 로그아웃 및 회원탈퇴 기능 추가 완료
    
    * feat: 이메일 중복 확인 처리 완료
    
    * feat: 회원가입 시 닉네임 중복 확인 처리 완료
    
    * fix: 회원가입 시 String 값에 대한 유효성 확인 주기 대폭 감소 처리

--- ANBDConsumer/ANBDConsumer/Presentaion/Authentication/View/SignUpEmailView.swift
+++ ANBDConsumer/ANBDConsumer/Presentaion/Authentication/View/SignUpEmailView.swift
@@ -13,58 +13,80 @@ struct SignUpEmailView: View {
     
     @FocusState private var focus: FocusableField?
     
-    @State private var navigate = false
+    @State private var isNavigate = false
+    @State private var isShowingDuplicatedEmailAlert = false
     
     var body: some View {
-        VStack {
-            Text("회원가입")
-                .font(ANBDFont.Heading2)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.top, 16)
-                .padding(.bottom, 70)
-            
-            TextFieldWithTitle(fieldType: .normal,
-                               title: "이메일 주소",
-                               placeholder: "예) anbd@anbd.co.kr",
-                               inputText: $authenticationViewModel.signUpEmailString)
-            .focused($focus, equals: .email)
-            .keyboardType(.emailAddress)
-            .textInputAutocapitalization(.never)
-            .autocorrectionDisabled(true)
-            .submitLabel(.go)
-            .onSubmit {
-                guard authenticationViewModel.isValidSignUpEmail else { return }
-                nextButtonAction()
-            }
-            
-            if !authenticationViewModel.errorMessage.isEmpty {
-                Text(authenticationViewModel.errorMessage)
+        ZStack {
+            VStack {
+                Text("회원가입")
+                    .font(ANBDFont.Heading2)
                     .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.top, 8)
-                    .font(ANBDFont.Caption1)
-                    .foregroundStyle(Color.heartRed)
-            }
-            
-            HStack {
-                Text("이미 계정이 있으신가요?")
-                    .foregroundStyle(.gray)
+                    .padding(.top, 16)
+                    .padding(.bottom, 70)
                 
-                Button("로그인") {
-                    dismiss()
+                TextFieldWithTitle(fieldType: .normal,
+                                   title: "이메일 주소",
+                                   placeholder: "예) anbd@anbd.co.kr",
+                                   inputText: $authenticationViewModel.signUpEmailString)
+                .focused($focus, equals: .email)
+                .keyboardType(.emailAddress)
+                .textInputAutocapitalization(.never)
+                .autocorrectionDisabled(true)
+                .submitLabel(.go)
+                .onSubmit {
+                    guard authenticationViewModel.isValidSignUpEmail else { return }
+                    
+                    Task {
+                        if await authenticationViewModel.checkDuplicatedEmail() {
+                            isShowingDuplicatedEmailAlert.toggle()
+                        } else {
+                            nextButtonAction()
+                        }
+                    }
+
                 }
-                .foregroundStyle(.accent)
+                
+                if !authenticationViewModel.errorMessage.isEmpty {
+                    Text(authenticationViewModel.errorMessage)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top, 8)
+                        .font(ANBDFont.Caption1)
+                        .foregroundStyle(Color.heartRed)
+                }
+                
+                HStack {
+                    Text("이미 계정이 있으신가요?")
+                        .foregroundStyle(.gray)
+                    
+                    Button("로그인") {
+                        dismiss()
+                    }
+                    .foregroundStyle(.accent)
+                }
+                .font(ANBDFont.body2)
+                .padding(.top, 22)
+                
+                Spacer()
+                
+                BlueSquareButton(title: "다음", isDisabled: !authenticationViewModel.isValidSignUpEmail) {
+                    Task {
+                        if await authenticationViewModel.checkDuplicatedEmail() {
+                            isShowingDuplicatedEmailAlert.toggle()
+                        } else {
+                            nextButtonAction()
+                        }
+                    }
+                }
             }
-            .font(ANBDFont.body2)
-            .padding(.top, 22)
+            .padding()
             
-            Spacer()
-            
-            BlueSquareButton(title: "다음", isDisabled: !authenticationViewModel.isValidSignUpEmail) {
-                nextButtonAction()
+            if isShowingDuplicatedEmailAlert {
+                CustomAlertView(isShowingCustomAlert: $isShowingDuplicatedEmailAlert, viewType: .duplicatedEmail) {
+                    focus = .email
+                }
             }
         }
-        .padding()
-        
         .toolbar {
             ToolbarItemGroup(placement: .keyboard) {
                 Spacer()
@@ -77,7 +99,7 @@ struct SignUpEmailView: View {
             }
         }
         
-        .navigationDestination(isPresented: $navigate) {
+        .navigationDestination(isPresented: $isNavigate) {
             SignUpPasswordView()
         }
         
@@ -87,7 +109,7 @@ struct SignUpEmailView: View {
     }
     
     private func nextButtonAction() {
-        navigate = true
+        isNavigate = true
     }
 }
 

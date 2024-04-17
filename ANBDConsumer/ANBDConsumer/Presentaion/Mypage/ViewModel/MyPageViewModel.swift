9035f3cb367a567ff503185fcd8f822c176ed835
Seongmin Kim <sjybext@naver.com>
2024-04-17 22:18:36 +0900

    feat: 마이페이지 유저 정보 띄우기 완료 (#170)
    * feat: 마이페이지에서 로그인한 유저 정보 나타내기 완료
    
    * feat: 유저 정보 수정을 위한 사전 작업 완료
    
    * style: 불필요한 목데이터 삭제

--- ANBDConsumer/ANBDConsumer/Presentaion/Mypage/ViewModel/MyPageViewModel.swift
+++ ANBDConsumer/ANBDConsumer/Presentaion/Mypage/ViewModel/MyPageViewModel.swift
@@ -23,18 +23,20 @@ final class MyPageViewModel: ObservableObject {
                                likeArticles: [],
                                likeTrades: [])
     
+    private let userUsecase: UserUsecase = DefaultUserUsecase()
+    
     @Published var userProfileImage: UIImage = UIImage(named: "DefaultUserProfileImage.001.png")!
     
-    @Published var user = User(id: "abcd1234",
-                               nickname: "김마루",
-                               profileImage: "DefaultUserProfileImage",
-                               email: "sjybext@naver.com",
-                               favoriteLocation: .jeju,
+    @Published var user = User(id: "",
+                               nickname: "",
+                               profileImage: "",
+                               email: "",
+                               favoriteLocation: .seoul,
                                userLevel: .consumer,
-                               isOlderThanFourteen: true,
-                               isAgreeService: true,
-                               isAgreeCollectInfo: true,
-                               isAgreeMarketing: true,
+                               isOlderThanFourteen: false,
+                               isAgreeService: false,
+                               isAgreeCollectInfo: false,
+                               isAgreeMarketing: false,
                                likeArticles: [],
                                likeTrades: [])
     
@@ -44,18 +46,34 @@ final class MyPageViewModel: ObservableObject {
     @Published private(set) var userLikedArticles: [Article] = []
     @Published private(set) var userHeartedTrades: [Trade] = []
     
-    let mockArticleData: [Article] = [
-    ]
-    
-    let mockTradeData: [Trade] = [
-    ]
-    
     @Published var editedUserNickname = ""
     @Published var tempUserFavoriteLocation: Location = .seoul
     
     @Published var myPageNaviPath = NavigationPath()
     
-    func validateEditingComplete() -> Bool {
+    /// UserDefaults에서 유저 정보 불러오기
+    func loadUserInfo() {
+        if let user = UserDefaultsClient.shared.userInfo {
+            self.user = user
+        }
+    }
+    
+    /// 유저 정보 수정하기
+    func updateUserInfo(updatedNickname: String, updatedLocation: Location) async {
+        do {
+            var updatedUser: User = user
+            
+            updatedUser.nickname = updatedNickname
+            updatedUser.favoriteLocation = updatedLocation
+            
+            try await userUsecase.updateUserInfo(user: updatedUser)
+            UserDefaultsClient.shared.userInfo = updatedUser
+        } catch {
+            print("\(error.localizedDescription)")
+        }
+    }
+    
+    func validateUpdatingComplete() -> Bool {
         if (editedUserNickname.isEmpty || editedUserNickname == self.user.nickname) && (tempUserFavoriteLocation != self.user.favoriteLocation) {
             return true
         } else {

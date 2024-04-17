7475444cec088d4d1fe0a8b43eae03ea29035f29
Jiho <yjh7827@naver.com>
2024-04-17 11:44:44 +0900

    feat: Storage Error 정의

--- ANBDModel/Sources/Domain/Error/StorageError.swift
+++ ANBDModel/Sources/Domain/Error/StorageError.swift
@@ -7,9 +7,24 @@
 
 import Foundation
 
-public enum StorageError: Error {
+public enum StorageError: Int, Error {
+    case invalidContainerID = 5029
+    case invalidImagePath
+    case invalidImageData
     case uploadError
     case downloadError
     case updateError
     case deleteError
+    
+    public var message: String {
+        switch self {
+        case .invalidContainerID: "ContainerID 누락"
+        case .invalidImagePath: "ImagePath 누락"
+        case .invalidImageData: "ImageData 누락"
+        case .uploadError: "이미지 업로드 실패"
+        case .downloadError: "이미지 다운로드 실패"
+        case .updateError: "이미지 수정 실패"
+        case .deleteError: "이미지 삭제 실패"
+        }
+    }
 }

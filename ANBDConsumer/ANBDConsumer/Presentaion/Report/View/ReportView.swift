e079738b5491fff2d178cfe1a315ea6695534b35
최정인 <wjddls980912@naver.com>
2024-04-20 20:15:08 +0900

    feat: 신고하기 CustomAlertView 추가

--- ANBDConsumer/ANBDConsumer/Presentaion/Report/View/ReportView.swift
+++ ANBDConsumer/ANBDConsumer/Presentaion/Report/View/ReportView.swift
@@ -11,67 +11,78 @@ import ANBDModel
 struct ReportView: View {
     
     @EnvironmentObject private var reportViewModel: ReportViewModel
+    @Environment(\.dismiss) private var dismiss
     
     var reportViewType: ReportType = .article
     var reportedObjectID: String
     var reportedChannelID: String?
     @State private var reportReason: String = ""
+    @State private var isShowingCustomAlert: Bool = false
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("\(reportTitle) 신고하기")
-                .foregroundStyle(.gray900)
-                .font(ANBDFont.SubTitle1)
-            
-            VStack(alignment: .trailing) {
-                ZStack(alignment: .topLeading) {
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(.clear)
-                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(.gray400))
-                        
-                    
-                    TextEditor(text: $reportReason)
-                        .foregroundStyle(.gray900)
-                        .padding(13)
-                        
-                    
-                    if reportReason.isEmpty {
-                        Text("신고 내용을 입력해주세요. (최대 200자)")
-                            .padding()
-                            .padding(.top, 5)
-                    }
-                }
-                .frame(height: 200)
-                .font(ANBDFont.body1)
+        ZStack {
+            VStack(alignment: .leading) {
+                Text("\(reportTitle) 신고하기")
+                    .foregroundStyle(.gray900)
+                    .font(ANBDFont.SubTitle1)
                 
-                Text("\(reportReason.count)/200")
-                    .padding(.horizontal, 5)
-                    .font(ANBDFont.body2)
+                VStack(alignment: .trailing) {
+                    ZStack(alignment: .topLeading) {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.clear)
+                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(.gray400))
+                            
+                        
+                        TextEditor(text: $reportReason)
+                            .foregroundStyle(.gray900)
+                            .padding(13)
+                            
+                        
+                        if reportReason.isEmpty {
+                            Text("신고 내용을 입력해주세요. (최대 200자)")
+                                .padding()
+                                .padding(.top, 5)
+                        }
+                    }
+                    .frame(height: 200)
+                    .font(ANBDFont.body1)
+                    
+                    Text("\(reportReason.count)/200")
+                        .padding(.horizontal, 5)
+                        .font(ANBDFont.body2)
+                }
+                .foregroundStyle(.gray400)
+                .padding(.vertical, 10)
+                
+                Spacer()
+                
+                Button(action: {
+                    if !reportReason.isEmpty {
+                        isShowingCustomAlert.toggle()
+                    }
+                }, label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(reportReason.isEmpty ? .gray200 : Color.accentColor)
+                        
+                        Text("신고하기")
+                            .foregroundStyle(.white)
+                    }
+                })
+                .disabled(reportReason.isEmpty)
+                .frame(height: 50)
             }
-            .foregroundStyle(.gray400)
-            .padding(.vertical, 10)
+            .padding()
             
-            Spacer()
-            
-            Button(action: {
-                if !reportReason.isEmpty {
+            if isShowingCustomAlert {
+                CustomAlertView(isShowingCustomAlert: $isShowingCustomAlert, viewType: .report) {
                     Task {
                         try await reportViewModel.submitReport(reportType: reportViewType, reportReason: reportReason, reportedObjectID: reportedObjectID, reportChannelID: reportedChannelID)
+                        dismiss()
                     }
                 }
-            }, label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(reportReason.isEmpty ? .gray200 : Color.accentColor)
-                    
-                    Text("신고하기")
-                        .foregroundStyle(.white)
-                }
-            })
-            .disabled(reportReason.isEmpty)
-            .frame(height: 50)
+            }
         }
-        .padding()
         .navigationTitle("신고하기")
         .navigationBarTitleDisplayMode(.inline)
         .toolbar(.hidden, for: .tabBar)

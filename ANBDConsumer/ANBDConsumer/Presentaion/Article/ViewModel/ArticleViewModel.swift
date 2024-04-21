18e14c9920844cd427b6c6580cdd0b1062520bcb
Gipyo Kim <103730885+rlvy0513@users.noreply.github.com>
2024-04-21 22:34:12 +0900

    feature: Article Model 연결 작업 (#207)
    * refactor: Article, TradeCell 합치는 작업
    
    * refactor: ArticleListCell 변경사항 수정
    
    * refactor: 안쓰는 코드들 삭제
    
    * feature: ArticleViewModel Model 추가
    
    * refactor: ArticleViewModel Model 수정
    
    * feature: Article 불러오기 추가
    
    * feature: Article 작성, 수정 추가
    
    isFirstAppear으로 뷰 전환 시 게시글 사라지는거 막아둠..
    
    * feature: 좋아요 기능 작업
    
    * fix: 안내문구 수정
    
    * fix: 게시글 작성 수정
    
    * feature: Detail 이미지 띄우기
    
    * feature: 이미지 여러개 올리기
    
    * fix: 필터링 관련 수정
    
    위아래 패딩 수정, 게시글/거래글 없을 경우 안보이게
    
    * fix: Create 부분 수정
    
    ArticleViewModel 수정
    QA 반영
    
    * feature: Comment 작업
    
    댓글 아직 이상하기 작성됨........
    + 댓글 수정 뷰 추가
    
    * fix: 컨플릭 해결
    
    * refactor: UserStore로 수정

--- ANBDConsumer/ANBDConsumer/Presentaion/Article/ViewModel/ArticleViewModel.swift
+++ ANBDConsumer/ANBDConsumer/Presentaion/Article/ViewModel/ArticleViewModel.swift
@@ -11,32 +11,53 @@ import ANBDModel
 @MainActor
 final class ArticleViewModel: ObservableObject {
     
-    private let articleUseCase: ArticleUsecase = DefaultArticleUsecase()
+    let articleUseCase: ArticleUsecase = DefaultArticleUsecase()
+    let commentUseCase: CommentUsecase = DefaultCommentUsecase()
+    
     private let storageManager = StorageManager.shared
-    @Published var articles: [Article] = []
-
+    
     @Published var articlePath: NavigationPath = NavigationPath()
     
-//    @Published var accuaArticle: Article = .init(writerID: "", writerNickname: "", category: .accua, title: "",content: "", imagePaths: [], thumbnailImagePath: )
-//    @Published var dasiArticle: Article = .init(writerID: "", writerNickname: "", category: .accua, title: "",con, thumbnailImagePath: <#String#>tent: "", imagePaths: [])
-//    @Published var comments: [Comment] = []
+    @Published private(set) var articles: [Article] = []
+    @Published private(set) var filteredArticles: [Article] = []
+    
+    @Published var article: Article = Article(id: "",
+                                              writerID: "",
+                                              writerNickname: "",
+                                              createdAt: Date(),
+                                              category: .accua,
+                                              title: "",
+                                              content: "",
+                                              thumbnailImagePath: "",
+                                              imagePaths: [],
+                                              likeCount: 0,
+                                              commentCount: 0)
+    
+    @Published private(set) var comments: [Comment] = []
+    
+    @Published var comment: Comment = Comment(id: "",
+                                              articleID: "",
+                                              writerID: "",
+                                              writerNickname: "",
+                                              writerProfileImageURL: "",
+                                              createdAt: Date(),
+                                              content: "")
 
+    @Published var commentText: String = ""
+    
     @Published var sortOption: ArticleOrder = .latest
     
-    @Published private(set) var filteredArticles: [Article] = []
     @Published private var isLiked: Bool = false
-
-    let mockArticleData: [Article] = [
-    ]
     
     init() {
-
+        
     }
     
     func filteringArticles(category: ANBDCategory) {
         filteredArticles = articles.filter({ $0.category == category })
     }
     
+    @MainActor
     func loadAllArticles() async {
         do {
             try await self.articles.append(contentsOf: articleUseCase.loadArticleList(limit: 10))
@@ -53,42 +74,99 @@ final class ArticleViewModel: ObservableObject {
         }
     }
     
-//    func filteringArticles(category: ANBDCategory) async {
-//        do {
-//            filteredArticles.append(
-//                contentsOf: try await articleUseCase.loadArticleList(category: category, by: .latest, limit: 10)
-//                )
-//            print(filteredArticles)
-//        } catch {
-//            print(error.localizedDescription)
-//        }
-//    } 
-    
+    @MainActor
     func refreshSortedArticleList(category: ANBDCategory, by order: ArticleOrder, limit: Int) async {
         do {
-            filteredArticles.append(
-                contentsOf:  try await articleUseCase.refreshSortedArticleList(category: category, by: sortOption, limit: 10)
-                )
+            filteredArticles.removeAll()
+            
+            let newArticles = try await articleUseCase.refreshSortedArticleList(category: category, by: order, limit: limit)
+            filteredArticles.append(contentsOf: newArticles)
+            
             print(filteredArticles)
         } catch {
             print(error.localizedDescription)
         }
     }
     
-    func writeArticle(article: Article, imageDatas: [Data]) async {
+    func loadDetailImages(path: StoragePath, containerID: String, imagePath: [String]) async throws -> [Data] {
+        var detailImages: [Data] = []
+        
+        
+        for image in imagePath {
+            do {
+                detailImages.append(
+                    try await storageManager.downloadImage(path: path, containerID: containerID, imagePath: image)
+                )
+            } catch {
+                print("이미지 실패요... \(error.localizedDescription)")
+                
+                //이미지 예외
+                let image = UIImage(named: "ANBDWarning")
+                let imageData = image?.pngData()
+                detailImages.append( imageData ?? Data() )
+            }
+        }
+        return detailImages
+    }
+    
+    func writeArticle(category: ANBDCategory, title: String, content: String, imageDatas: [Data]) async {
+        let user = UserStore.shared.user
+        
+//        guard let user = UserDefaultsClient.shared.userInfo else {
+//            return
+//        }
+        // dump(article)
+        // dump(user)
+        let newArticle = Article(writerID: user.id,
+                                 writerNickname: user.nickname,
+                                 category: category,
+                                 title: title,
+                                 content: content,
+                                 thumbnailImagePath: "",
+                                 imagePaths: [])
+        
+        //이미지 리사이징
+        var newImages: [Data] = []
+        for image in imageDatas {
+            let imageData = await UIImage(data: image)?.byPreparingThumbnail(ofSize: .init(width: 1024, height: 1024))?.jpegData(compressionQuality: 0.5)
+            newImages.append(imageData ?? Data())
+        }
+        
         do {
-            try await articleUseCase.writeArticle(article: article, imageDatas: imageDatas)
-
+            try await articleUseCase.writeArticle(article: newArticle, imageDatas: newImages)
         } catch {
             print(error.localizedDescription)
         }
     }
     
-    func updateArticle(article: Article, imageDatas: [Data]) async {
+    func updateArticle(/*category: ANBDCategory, title: String, content: String*/article: Article, imageDatas: [Data]) async {
+        
+        //        self.article.category = category
+        //        self.article.title = title
+        //        self.article.content = content
+        
+        //이미지 리사이징
+        var newImages: [Data] = []
+        for image in imageDatas {
+            let imageData = await UIImage(data: image)?.byPreparingThumbnail(ofSize: .init(width: 1024, height: 1024))?.jpegData(compressionQuality: 0.5)
+            newImages.append(imageData ?? Data())
+        }
+        
         do {
-            try await articleUseCase.updateArticle(article: article, imageDatas: imageDatas)
+            try await articleUseCase.updateArticle(article: article, imageDatas: newImages)
+            //            article = try await articleUseCase.loadArticle(articleID: article.id)
         } catch {
             print(error.localizedDescription)
+        }
+    }
+    
+    func loadArticle(article: Article) async {
+        do {
+            let loadedArticle = try await articleUseCase.loadArticle(articleID: article.id)
+            self.article = loadedArticle
+        } catch {
+            print(error.localizedDescription)
+            print("실패실패실패실패실패")
         }
     }
     
@@ -146,6 +224,59 @@ final class ArticleViewModel: ObservableObject {
             return "좋아요순"
         case .mostComment:
             return "댓글순"
+        }
+    }
+    
+    // MARK: - Comment
+    func writeComment(articleID: String, content: String) async {
+        let user = UserStore.shared.user
+//        guard let user = UserDefaultsClient.shared.userInfo else {
+//            return
+//        }
+        
+        let newComment = Comment(articleID: articleID,
+                                 writerID: user.id,
+                                 writerNickname: user.nickname,
+                                 writerProfileImageURL: user.profileImage,
+                                 content: content)
+        
+        comments.append(newComment)
+        print("\(newComment.content)")
+        
+        do {
+            try await commentUseCase.writeComment(articleID: articleID, comment: newComment)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func loadCommentList(articleID: String) async {
+        do {
+            let loadedComment = try await commentUseCase.loadCommentList(articleID: articleID)
+            self.comments = loadedComment
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func updateComment(comment: Comment) async {
+        do {
+            try await commentUseCase.updateComment(comment: comment)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func deleteComment(articleID: String, commentID: String) async {
+        do {
+            try await commentUseCase.deleteComment(articleID: articleID, commentID: commentID)
+            print("articleID: \(article.id), \(articleID)")
+            print("commentID: \(comment.id), \(commentID)")
+        } catch {
+            print(error.localizedDescription)
+            print("댓글 삭제 실패")
+            print("articleID: \(article.id), \(articleID)")
+            print("commentID: \(comment.id), \(commentID)")
         }
     }
     

9b794c31013c14fdc087929d49325afdae3e8936
Jiho Yoo <49361214+jihoooo97@users.noreply.github.com>
2024-04-22 14:42:32 +0900

    feat: 글 [쓰기, 삭제] 시 ANBD 카운트 작업 (#224)
    * feat: User ANBC Count필드 추가
    
    * feat: 글 [쓰기, 삭제] 시ANBC Count 증감 작업

--- ANBDModel/Sources/Domain/VO/User.swift
+++ ANBDModel/Sources/Domain/VO/User.swift
@@ -41,6 +41,18 @@ public struct User: Codable, Identifiable, Hashable {
     /// 찜한 나눠쓰기, 바꿔쓰기 목록
     public var likeTrades: [String]
     
+    /// 작성한 아껴쓰기 글 갯수
+    public var accuaCount: Int
+    
+    /// 작성한 나눠쓰기 글 갯수
+    public var nanuaCount: Int
+    
+    /// 작성한 바꿔쓰기 글 갯수
+    public var baccuaCount: Int
+    
+    /// 작성한 다시쓰기 글 갯수
+    public var dasiCount: Int
+    
     
     public init(
         id: String,
@@ -54,7 +66,11 @@ public struct User: Codable, Identifiable, Hashable {
         isAgreeCollectInfo: Bool,
         isAgreeMarketing: Bool, 
         likeArticles: [String] = [],
-        likeTrades: [String] = []
+        likeTrades: [String] = [],
+        accuaCount: Int = 0,
+        nanuaCount: Int = 0,
+        baccuaCount: Int = 0,
+        dasiCount: Int = 0
     ) {
         self.id = id
         self.nickname = nickname
@@ -68,5 +84,9 @@ public struct User: Codable, Identifiable, Hashable {
         self.isAgreeMarketing = isAgreeMarketing
         self.likeArticles = likeArticles
         self.likeTrades = likeTrades
+        self.accuaCount = accuaCount
+        self.nanuaCount = nanuaCount
+        self.baccuaCount = baccuaCount
+        self.dasiCount = dasiCount
     }
 }

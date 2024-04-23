ec6ac44e69a65c44aea642218ac6f71e3f3505da
Sim sangwon <epepep0938@gmail.com>
2024-04-23 14:38:17 +0900

    add: articleLisyView 페이지네이션 구현
    add: articleLisyView 페이지네이션 구현

--- ANBDAdmin/ANBDAdmin/Presentaion/Article/ArticleListViewModel.swift
+++ ANBDAdmin/ANBDAdmin/Presentaion/Article/ArticleListViewModel.swift
@@ -12,6 +12,8 @@ class ArticleListViewModel: ObservableObject {
     @Published var articleList: [Article] = []
     var deletedArticleID: String?
     let articleUsecase = DefaultArticleUsecase()
+    @Published var canLoadMore: Bool = true
+
     
     func firstLoadArticles() {
         if articleList.isEmpty {
@@ -27,6 +29,24 @@ class ArticleListViewModel: ObservableObject {
             }
         }
     }
+    func loadMoreArticles(){ //10개씩 불러옴
+        guard canLoadMore else { return }
+
+        Task {
+            do {
+                let articles = try await articleUsecase.loadArticleList()
+                DispatchQueue.main.async {
+                    self.articleList.append(contentsOf: articles)
+                    if articles.count < 10 {
+                        self.canLoadMore = false
+                    }
+                }
+            } catch {
+                print("게시물 목록을 가져오는데 실패했습니다: \(error)")
+            }
+        }
+    }
+
     func loadArticle(articleID: String) async throws -> Article {
         return try await articleUsecase.loadArticle(articleID: articleID)
     }

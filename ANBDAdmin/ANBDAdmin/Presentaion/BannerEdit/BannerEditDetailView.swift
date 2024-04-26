e8d00eba77fac3a57b798ac04609ccb2088e8ed5
Sim sangwon <epepep0938@gmail.com>
2024-04-26 19:51:21 +0900

    refactor: debug용 print문 삭제
    refactor: debug용 print문 삭제

--- ANBDAdmin/ANBDAdmin/Presentaion/BannerEdit/BannerEditDetailView.swift
+++ ANBDAdmin/ANBDAdmin/Presentaion/BannerEdit/BannerEditDetailView.swift
@@ -28,9 +28,6 @@ struct BannerEditDetailView: View {
                     }
                     .setProcessor(DownsamplingImageProcessor(size: CGSize(width: 660, height: 220)))
                     .fade(duration: 1)
-                    .onSuccess { r in
-                        print("Task done for: \(r.source.url?.absoluteString ?? "")")
-                    }
                     .onFailure { e in
                         print("Job failed: \(e.localizedDescription)")
                     }

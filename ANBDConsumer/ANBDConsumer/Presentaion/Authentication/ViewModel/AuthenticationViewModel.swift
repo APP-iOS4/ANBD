02a6d59e32077e03fcd0b1bbed9d79fd76f72480
Kimseongmin <sjybext@naver.com>
2024-04-26 11:16:09 +0900

    refactor: 중복되는 Extension 삭제

--- ANBDConsumer/ANBDConsumer/Presentaion/Authentication/ViewModel/AuthenticationViewModel.swift
+++ ANBDConsumer/ANBDConsumer/Presentaion/Authentication/ViewModel/AuthenticationViewModel.swift
@@ -375,21 +375,3 @@ extension AuthenticationViewModel {
         isValidSignUp = false
     }
 }
-
-extension String {
-    func isValidateEmail() -> Bool {
-        let emailRegEx = #"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,64}"#
-        let last = self.contains("com") || self.contains("net") || self.contains("co.kr")
-        return NSPredicate(format: "SELF MATCHES %@", emailRegEx).evaluate(with: self) && last
-    }
-    
-    func isValidatePassword() -> Bool {
-        let regex = #"^(?!([A-Za-z]+|[~!@#$%^&*()_+=]+|[0-9]+)$)[A-Za-z\d~!@#$%^&*()_+=]{8,16}$"#
-        return NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: self)
-    }
-    
-    func isValidateNickname() -> Bool {
-        let regex = #"^[0-9a-zA-Z가-힣][0-9a-zA-Z가-힣._]{0,18}[0-9a-zA-Z가-힣]$"#
-        return NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: self)
-    }
-}

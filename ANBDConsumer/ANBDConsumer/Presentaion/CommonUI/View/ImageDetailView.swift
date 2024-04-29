b63e67b84fcc1430e8248c3b4b07f2d706e544e4
최정인 <wjddls980912@naver.com>
2024-04-30 08:13:35 +0900

    refactor: 마루 QA 반영

--- ANBDConsumer/ANBDConsumer/Presentaion/CommonUI/View/ImageDetailView.swift
+++ ANBDConsumer/ANBDConsumer/Presentaion/CommonUI/View/ImageDetailView.swift
@@ -16,47 +16,62 @@ struct ImageDetailView: View {
     @State private var totalZoom = 1.0
     
     var body: some View {
-        VStack {
+        ZStack(alignment: .top) {
+            VStack {
+                Spacer()
+                
+                TabView(selection: $idx) {
+                    ForEach(0..<images.count, id: \.self) { i in
+                        if let image = UIImage(data: images[i]) {
+                            Image(uiImage: image)
+                                .resizable()
+                                .scaledToFit()
+                                .scaleEffect(currentZoom + totalZoom)
+                                .gesture(
+                                    MagnificationGesture()
+                                        .onChanged { value in
+                                            if totalZoom >= 1.0 {
+                                                currentZoom = value - 1
+                                            }
+                                        }
+                                        .onEnded { value in
+                                            totalZoom += currentZoom
+                                            currentZoom = 0
+                                            if totalZoom < 1.0 {
+                                                totalZoom = 1.0
+                                                currentZoom = 0.0
+                                            }
+                                        }
+                                )
+                                .onAppear {
+                                    currentZoom = 0.0
+                                    totalZoom = 1.0
+                                }
+                        } else {
+                            ProgressView()
+                        }
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle())
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .automatic))
+                
+                Spacer()
+            }
+            .ignoresSafeArea(.all)
+            
             HStack {
                 Button(action: {
                     isShowingImageDetailView.toggle()
                 }, label: {
                     Image(systemName: "xmark")
+                        .resizable()
+                        .frame(width: 18, height: 18)
                         .foregroundStyle(.white)
                 })
-                .padding()
+                .padding(18)
                 
                 Spacer()
             }
-            
-            Spacer()
-            
-            TabView(selection: $idx) {
-                ForEach(0..<images.count, id: \.self) { i in
-                    if let image = UIImage(data: images[i]) {
-                        Image(uiImage: image)
-                            .resizable()
-                            .scaledToFit()
-                            .scaleEffect(currentZoom + totalZoom)
-                            .gesture(
-                                MagnificationGesture()
-                                    .onChanged { value in
-                                        currentZoom = value - 1
-                                    }
-                                    .onEnded { value in
-                                        totalZoom += currentZoom
-                                        currentZoom = 0
-                                    }
-                            )
-                    } else {
-                        ProgressView()
-                    }
-                }
-            }
-            .tabViewStyle(PageTabViewStyle())
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .automatic))
-            
-            Spacer()
         }
         .background(.black.opacity(0.95))
         .gesture(drag)

4ed17d47e080129b7ec67dab59b130e4f349af3a
JURI <80569323+juri123123@users.noreply.github.com>
2024-04-30 09:27:13 +0900

    refactor: 코드리뷰 반영

--- ANBDConsumer/ANBDConsumer/Presentaion/CommonUI/View/LoadingView.swift
+++ ANBDConsumer/ANBDConsumer/Presentaion/CommonUI/View/LoadingView.swift
@@ -8,8 +8,6 @@
 import SwiftUI
 
 struct LoadingView: View {
-    @EnvironmentObject private var coordinator: Coordinator
-    
     var body: some View {
         VStack {
             ProgressView()

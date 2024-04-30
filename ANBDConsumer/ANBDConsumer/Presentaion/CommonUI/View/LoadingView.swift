a3839a76a6cb44a489ba98de7a05ee5128b03afa
JURI <80569323+juri123123@users.noreply.github.com>
2024-04-30 09:29:18 +0900

    refactor: 사진 업데이트 시 바로 반영 (#299)
    * refactor: 사진 업데이트 시 바로 반영
    
    * refactor: 코드리뷰 반영

---
+++ ANBDConsumer/ANBDConsumer/Presentaion/CommonUI/View/LoadingView.swift
@@ -0,0 +1,26 @@
+//
+//  LoadingView.swift
+//  ANBDConsumer
+//
+//  Created by 최주리 on 4/29/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        VStack {
+            ProgressView()
+            Text("Loading")
+                .font(ANBDFont.body1)
+                .fontWeight(.semibold)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.white.opacity(0.5))
+    }
+}
+
+#Preview {
+    LoadingView()
+}

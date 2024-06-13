05bc2d2c76dd04be3e756c0f81eac971c75b9ee0
JURI <80569323+juri123123@users.noreply.github.com>
2024-06-13 16:18:12 +0900

    feat: collectview

--- ANBDUIKit/ANBDUIKit/Presentation/Home/HomeViewController.swift
+++ ANBDUIKit/ANBDUIKit/Presentation/Home/HomeViewController.swift
@@ -10,6 +10,13 @@ import SnapKit
 
 class HomeViewController: UIViewController {
     
+    // sample
+    private var dataSource: [UIImage?] = {
+        (1...5).map { _ in return UIImage(named: "sampleImage")}
+    }()
+    
+    private lazy var moreButtonConfig = UIButton.Configuration.borderless()
+    
     private lazy var searchButton = UIBarButtonItem()
     private lazy var scrollView = UIScrollView()
     private lazy var contentStackView = UIStackView()
@@ -20,26 +27,34 @@ class HomeViewController: UIViewController {
     private lazy var baccuaStackView = UIStackView()
     private lazy var dasiStackView = UIStackView()
     
+    private lazy var accuaTitleStackView = UIStackView()
     private lazy var accuaTitleLabel = UILabel()
     private lazy var accuaDescriptionLabel = UILabel()
     private lazy var accuaImageView = UIImageView()
     private var accuaDividerView = UIView()
     private var accuaMoreButton = UIButton()
     
+    private lazy var nanuaTitleStackView = UIStackView()
     private lazy var nanuaTitleLabel = UILabel()
     private lazy var nanuaDescriptionLabel = UILabel()
     private var nanuaDividerView = UIView()
     private lazy var nanuaImageStackView = UIStackView()
     private lazy var nanuaImageView = UIImageView()
     private lazy var nanuaScrollView = UIScrollView()
+    private lazy var nanuaCollectionView = UICollectionView()
+    private var nanuaMoreButton = UIButton()
     
+    private lazy var baccuaTitleStackView = UIStackView()
     private lazy var baccuaTitleLabel = UILabel()
     private lazy var baccuaDescriptionLabel = UILabel()
     private var baccuaDividerView = UIView()
+    private var baccuaMoreButton = UIButton()
     
+    private lazy var dasiTitleStackView = UIStackView()
     private lazy var dasiTitleLabel = UILabel()
     private lazy var dasiDescriptionLabel = UILabel()
     private lazy var dasiImageView = UIImageView()
+    private var dasiMoreButton = UIButton()
     
     
     override func viewDidLoad() {
@@ -48,6 +63,7 @@ class HomeViewController: UIViewController {
         
         initAttribute()
         addView()
+        setCollectionView()
         initLayout()
     }
     
@@ -55,34 +71,42 @@ class HomeViewController: UIViewController {
         searchButton.image = UIImage(systemName: "magnifyingglass")
         navigationItem.rightBarButtonItem = searchButton
         
+        moreButtonConfig.title = "더보기"
+        moreButtonConfig.image = UIImage(systemName: "chevron.right")
+        moreButtonConfig.imagePlacement = .trailing
+        moreButtonConfig.imagePadding = 5
+        moreButtonConfig.baseForegroundColor = .black
+        moreButtonConfig.buttonSize = .mini
+        
         scrollView = {
             let scrollView = UIScrollView()
+            scrollView.showsVerticalScrollIndicator = false
+            
             return scrollView
         }()
         contentStackView = {
             let stackView = UIStackView()
             stackView.axis = .vertical
-//            stackView.distribution = .equalCentering
-            stackView.spacing = 100
+            stackView.spacing = 15
             
             return stackView
         }()
         
         accuaDividerView = {
             let view = UIView()
-            view.backgroundColor = .lightGray
+            view.backgroundColor = .systemGray5
             
             return view
         }()
         nanuaDividerView = {
             let view = UIView()
-            view.backgroundColor = .lightGray
+            view.backgroundColor = .systemGray5
             
             return view
         }()
         baccuaDividerView = {
             let view = UIView()
-            view.backgroundColor = .lightGray
+            view.backgroundColor = .systemGray5
             
             return view
         }()
@@ -102,28 +126,41 @@ class HomeViewController: UIViewController {
         accuaStackView = {
             let stackView = UIStackView()
             stackView.axis = .vertical
+            stackView.spacing = 13
             return stackView
         }()
         nanuaStackView = {
             let stackView = UIStackView()
             stackView.axis = .vertical
+            stackView.spacing = 13
             return stackView
         }()
         baccuaStackView = {
             let stackView = UIStackView()
             stackView.axis = .vertical
+            stackView.spacing = 13
             return stackView
         }()
         dasiStackView = {
             let stackView = UIStackView()
             stackView.axis = .vertical
+            stackView.spacing = 13
             return stackView
         }()
         
+        accuaTitleStackView = {
+            let stackView = UIStackView()
+            stackView.axis = .horizontal
+            return stackView
+        }()
+        accuaMoreButton = {
+           let button = UIButton(configuration: moreButtonConfig)
+            return button
+        }()
         accuaTitleLabel = {
             let label = UILabel()
             label.text = "아껴쓰기"
-            label.font = UIFont.preferredFont(forTextStyle: .title2)
+            label.font = UIFont.boldSystemFont(ofSize: 22)
             
             return label
         }()
@@ -144,17 +181,20 @@ class HomeViewController: UIViewController {
             imageView.clipsToBounds = true
             return imageView
         }()
-        accuaMoreButton = {
-            let button = UIButton()
-            button.setTitle("더보기", for: .normal)
-            button.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+        
+        nanuaTitleStackView = {
+            let stackView = UIStackView()
+            stackView.axis = .horizontal
+            return stackView
+        }()
+        nanuaMoreButton = {
+           let button = UIButton(configuration: moreButtonConfig)
             return button
         }()
-        
         nanuaTitleLabel = {
             let label = UILabel()
             label.text = "나눠쓰기"
-            label.font = UIFont.preferredFont(forTextStyle: .title2)
+            label.font = UIFont.boldSystemFont(ofSize: 22)
             
             return label
         }()
@@ -164,28 +204,32 @@ class HomeViewController: UIViewController {
             
             return label
         }()
-        nanuaImageView = {
-            let imageView = UIImageView()
-            let image = UIImage(named: "sampleImage")
-            let resizedImage = image?.resized(to: CGSize(width: 35, height: 35))
-            imageView.layer.cornerRadius = 10
-            imageView.image = resizedImage
-            imageView.contentMode = .scaleAspectFit
+        nanuaCollectionView = {
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .horizontal
+            layout.itemSize = CGSize(width: 150, height: 150)
             
-            imageView.clipsToBounds = true
-            return imageView
-        }()
-        nanuaImageStackView = {
-            let stackView = UIStackView()
-            stackView.axis = .horizontal
-            
-            return stackView
+            let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+            cv.isScrollEnabled = true
+            cv.showsHorizontalScrollIndicator = false
+            cv.contentInset = .zero
+            cv.backgroundColor = .clear
+            return cv
         }()
         
+        baccuaTitleStackView = {
+            let stackView = UIStackView()
+            stackView.axis = .horizontal
+            return stackView
+        }()
+        baccuaMoreButton = {
+           let button = UIButton(configuration: moreButtonConfig)
+            return button
+        }()
         baccuaTitleLabel = {
             let label = UILabel()
             label.text = "바꿔쓰기"
-            label.font = UIFont.preferredFont(forTextStyle: .title2)
+            label.font = UIFont.boldSystemFont(ofSize: 22)
             
             return label
         }()
@@ -196,10 +240,19 @@ class HomeViewController: UIViewController {
             return label
         }()
         
+        dasiTitleStackView = {
+            let stackView = UIStackView()
+            stackView.axis = .horizontal
+            return stackView
+        }()
+        dasiMoreButton = {
+           let button = UIButton(configuration: moreButtonConfig)
+            return button
+        }()
         dasiTitleLabel = {
             let label = UILabel()
             label.text = "다시쓰기"
-            label.font = UIFont.preferredFont(forTextStyle: .title2)
+            label.font = UIFont.boldSystemFont(ofSize: 22)
             
             return label
         }()
@@ -232,35 +285,31 @@ class HomeViewController: UIViewController {
             contentStackView.addArrangedSubview($0)
         }
         
-        [accuaTitleLabel, accuaDescriptionLabel, accuaImageView, accuaDividerView].forEach {
+        [accuaTitleLabel, accuaMoreButton].forEach {
+            accuaTitleStackView.addArrangedSubview($0)
+        }
+        [accuaTitleStackView, accuaDescriptionLabel, accuaImageView, accuaDividerView].forEach {
             accuaStackView.addArrangedSubview($0)
         }
-        
-        for i in 0..<5 {
-            nanuaImageView = {
-                let imageView = UIImageView()
-                let image = UIImage(named: "sampleImage")
-                let resizedImage = image?.resized(to: CGSize(width: 35, height: 35))
-                imageView.layer.cornerRadius = 10
-                imageView.image = resizedImage
-                imageView.contentMode = .scaleAspectFit
-                
-                imageView.clipsToBounds = true
-                return imageView
-            }()
-            
-            nanuaImageStackView.addArrangedSubview(nanuaImageView)
+
+        [nanuaTitleLabel, nanuaMoreButton].forEach {
+            nanuaTitleStackView.addArrangedSubview($0)
         }
-        
-        nanuaScrollView.addSubview(nanuaImageStackView)
-        
-        [nanuaTitleLabel, nanuaDescriptionLabel, nanuaScrollView, nanuaDividerView].forEach {
+        [nanuaTitleStackView, nanuaDescriptionLabel, nanuaCollectionView, nanuaDividerView].forEach {
             nanuaStackView.addArrangedSubview($0)
         }
-        [baccuaTitleLabel, baccuaDescriptionLabel, baccuaDividerView].forEach {
+        
+        [baccuaTitleLabel, baccuaMoreButton].forEach {
+            baccuaTitleStackView.addArrangedSubview($0)
+        }
+        [baccuaTitleStackView, baccuaDescriptionLabel, baccuaDividerView].forEach {
             baccuaStackView.addArrangedSubview($0)
         }
-        [dasiTitleLabel, dasiDescriptionLabel, dasiImageView].forEach {
+        
+        [dasiTitleLabel, dasiMoreButton].forEach {
+            dasiTitleStackView.addArrangedSubview($0)
+        }
+        [dasiTitleStackView, dasiDescriptionLabel, dasiImageView].forEach {
             dasiStackView.addArrangedSubview($0)
         }
     }
@@ -282,86 +331,57 @@ class HomeViewController: UIViewController {
             $0.horizontalEdges.equalTo(contentStackView)
             $0.top.equalTo(contentStackView.snp.top)
         }
-        accuaStackView.snp.makeConstraints {
-            $0.top.equalTo(commerceImageView.snp.bottom)
-            $0.width.equalTo(contentStackView.snp.width)
-            $0.height.equalTo(200)
+        
+        nanuaCollectionView.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.height.equalTo(150)
         }
         
-        nanuaStackView.snp.makeConstraints {
-            $0.top.equalTo(accuaStackView.snp.bottom)
-            $0.width.equalTo(contentStackView.snp.width)
-            //$0.height.equalTo(200)
-        }
-        
-        baccuaStackView.snp.makeConstraints {
-            $0.top.equalTo(nanuaStackView.snp.bottom)
-            $0.width.equalTo(scrollView.snp.width)
-            //$0.height.equalTo(200)
-        }
-        
-        dasiStackView.snp.makeConstraints {
-            $0.top.equalTo(baccuaStackView.snp.bottom)
-            $0.width.equalTo(scrollView.snp.width)
-            $0.height.equalTo(200)
-        }
-        
-        accuaTitleLabel.snp.makeConstraints {
-            $0.top.equalTo(accuaStackView.snp.top)
-        }
-        accuaDescriptionLabel.snp.makeConstraints {
-            $0.top.equalTo(accuaTitleLabel.snp.bottom)
-        }
-        accuaImageView.snp.makeConstraints {
-            $0.top.equalTo(accuaDescriptionLabel.snp.bottom)
-        }
         accuaDividerView.snp.makeConstraints {
-            $0.top.equalTo(accuaImageView.snp.bottom)
             $0.height.equalTo(1)
-            $0.width.equalTo(accuaStackView.snp.width)
         }
         
-        nanuaTitleLabel.snp.makeConstraints {
-            $0.top.equalTo(nanuaStackView.snp.top)
-        }
-        nanuaDescriptionLabel.snp.makeConstraints {
-            $0.top.equalTo(nanuaTitleLabel.snp.bottom)
-        }
-        nanuaScrollView.snp.makeConstraints {
-            $0.top.equalTo(nanuaDescriptionLabel.snp.bottom)
-            $0.left.right.equalTo(nanuaStackView)
-            $0.height.equalTo(40)
-        }
-        nanuaImageStackView.snp.makeConstraints {
-            $0.top.equalTo(nanuaDescriptionLabel.snp.bottom)
-        }
         nanuaDividerView.snp.makeConstraints {
-            //$0.top.equalTo(nanuaDescriptionLabel.snp.bottom)
             $0.height.equalTo(1)
-            $0.width.equalTo(nanuaStackView.snp.width)
         }
         
-        baccuaTitleLabel.snp.makeConstraints {
-            $0.top.equalTo(baccuaStackView.snp.top)
-        }
-        baccuaDescriptionLabel.snp.makeConstraints {
-            $0.top.equalTo(baccuaTitleLabel.snp.bottom)
-        }
         baccuaDividerView.snp.makeConstraints {
-            $0.top.equalTo(baccuaDescriptionLabel.snp.bottom)
             $0.height.equalTo(1)
-            $0.width.equalTo(baccuaStackView.snp.width)
         }
-        
-        dasiTitleLabel.snp.makeConstraints {
-            $0.top.equalTo(dasiStackView.snp.top)
-        }
-        dasiDescriptionLabel.snp.makeConstraints {
-            $0.top.equalTo(dasiTitleLabel.snp.bottom)
-        }
-        dasiImageView.snp.makeConstraints {
-            $0.top.equalTo(dasiDescriptionLabel.snp.bottom)
-        }
+    }
+    
+    func setCollectionView() {
+        nanuaCollectionView.register(NanuaCollectionViewCell.self, forCellWithReuseIdentifier: "nanuaCell")
+        nanuaCollectionView.dataSource = self
+        nanuaCollectionView.delegate = self
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    // 셀 사이의 간격
+    func collectionView( _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    // 셀과 뷰의 간격
+    func collectionView( _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 10)
+    }
+    
+    // 셀이 눌렸을 때
+    //    func collectionView( _ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    //    }
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "nanuaCell", for: indexPath) as! NanuaCollectionViewCell
+        cell.prepare(image: dataSource[indexPath.item])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataSource.count
     }
 }
 
---
+++ ANBDUIKit/ANBDUIKit/Presentation/Home/NanuaCollectionViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  NanuaCollectionViewCell.swift
+//  ANBDUIKit
+//
+//  Created by 최주리 on 6/11/24.
+//
+
+import UIKit
+
+final class NanuaCollectionViewCell: UICollectionViewCell {
+    static let identifier = "nanuaCell"
+    
+    private var image: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 15
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.contentView.addSubview(image)
+        
+        image.snp.makeConstraints {
+            $0.leading.trailing.top.bottom.equalTo(contentView)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        self.prepare(image: nil)
+    }
+    
+    func prepare(image: UIImage?) {
+        self.image.image = image
+    }
+}
